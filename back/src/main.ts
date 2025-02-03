4ba95c10655ce6ba822f00e33c0da7787d059f83
Thibault YOU <thibaultyou@gmail.com>
2025-02-03 16:13:21 +0100

    🔧 chore(nginx): Update config for Swagger redirection

--- back/src/main.ts
+++ back/src/main.ts
@@ -22,6 +22,7 @@ const bootstrap = async () => {
     .setTitle('Trading toolbox')
     .setVersion('1.0')
     .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT', name: 'Authorization', in: 'header' })
+    .addServer('/api')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(Urls.SWAGGER_DOCS, app, document);
